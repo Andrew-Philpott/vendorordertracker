f3810417b6eabf6d477e69170a828e60c15af93e
Andrew Philpott <andrewphilpott92@gmail.com>
2020-03-13 14:43:23 -0700

    Add styling to pages.

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -12,14 +12,14 @@ namespace VendorOrderTracker.Models
     public string Title { get; set; }
     public string Description { get; set; }
     public double Price { get; set; } = 0;
-    public DateTime Date { get; set; }
+    public string Date { get; set; }
     public Vendor Vendor { get; set; }
 
     // Dictionary<string, string> bakedGoods = new Dictionary<string, string>();
     List<BakedGood> bakedGoods = new List<BakedGood>();
     public Order(string title, string description, params string[] list)
     {
-      Date = DateTime.Now;
+      Date = DateTime.Now.ToString("dd.MM.yyy");
       Id = AssignId++;
       Title = title;
       Description = description;
