c06faa4a2b5a5810db31cee125347236139fd04a
Andrew Philpott <andrewphilpott92@gmail.com>
2020-03-13 08:27:24 -0700

    Add html to home page index, vendor index and new pages.

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -2,6 +2,13 @@ namespace VendorOrderTracker.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
 
+    public Order(string title, string description, double price)
+    {
+
+    }
   }
 }
